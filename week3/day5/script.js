b795df72451b39fe53fda8b2c264b29ea756c8bb
Azar <azar.barfipour@gmail.com>
2021-09-24 09:10:05 -0700

    update isSorted func

---
+++ week3/day5/script.js
@@ -0,0 +1,23 @@
+function isSorted(arr) {
+  let isDescending = true;
+  let isAscending = true;
+
+  for (let i = 1; i < arr.length; i++) {
+    let v1 = arr[i - 1];
+    let v2 = arr[i];
+
+    if (v1 < v2) {
+      isDescending = false;
+    }
+
+    if (v1 > v2) {
+      isAscending = false;
+    }
+  }
+  return isAscending || isDescending;
+}
+
+console.log(isSorted([]));
+console.log(isSorted([1, 2, 3]));
+console.log(isSorted([3, 2, 1]));
+console.log(isSorted([3, 5, 1]));
