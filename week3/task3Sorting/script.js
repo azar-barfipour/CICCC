5ef7c8f082309d1a8b87fb2d1908ffd3c2d60cb6
Azar <azar.barfipour@gmail.com>
2021-09-23 17:56:29 -0700

    update functions to arrow functions

--- week3/task3Sorting/script.js
+++ week3/task3Sorting/script.js
@@ -1,38 +1,66 @@
-function isAscending(arr) {
+// function isAscending(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i - 1] > arr[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+const isAscending = (arr) => {
   for (let i = 1; i < arr.length; i++) {
     if (arr[i - 1] > arr[i]) {
       return false;
     }
   }
   return true;
-}
-function isDescending(arr) {
+};
+// function isDescending(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i - 1] < arr[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+const isDescending = (arr) => {
   for (let i = 1; i < arr.length; i++) {
     if (arr[i - 1] < arr[i]) {
       return false;
     }
   }
   return true;
-}
-function isSort(arr) {
-  //fisrt approch
-  if (isAscending(arr) === false && isDescending(arr) === false) {
-    return "Not Sorted";
-  } else if (isAscending(arr) === true) {
-    return "Ascending";
-  } else if (isDescending(arr) === true) {
-    return "Descending";
-  }
-}
+};
+// function isSort(arr) {
+//   //fisrt approch
+//   if (isAscending(arr) === false && isDescending(arr) === false) {
+//     return "Not Sorted";
+//   } else if (isAscending(arr) === true) {
+//     return "Ascending";
+//   } else if (isDescending(arr) === true) {
+//     return "Descending";
+//   }
+// }
 
-function isSort(arr) {
+// function isSort(arr) {
+//   //second approch
+//   if (isAscending(arr) === false && isDescending(arr) === false) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
+const isSort = (arr) => {
   //second approch
   if (isAscending(arr) === false && isDescending(arr) === false) {
     return false;
   } else {
     return true;
   }
-}
+};
+
 console.log(isSort([]));
 console.log(isSort([42]));
 console.log(isSort([39, 42]));
